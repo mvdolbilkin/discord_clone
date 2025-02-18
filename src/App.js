92a7440258ef3896573971488be891b6b6b0fc6b
mvdolbilkin <wasdny0@gmail.com>
2025-02-18 11:25:21 +0300

    ыфв

--- src/App.js
+++ src/App.js
@@ -4,25 +4,22 @@ import io from 'socket.io-client';
 const socket = io('http://85.192.25.173:8080', { transports: ['websocket', 'polling'] });
 
 function App() {
-    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState(localStorage.getItem('username') || '');
     const [password, setPassword] = useState('');
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [token, setToken] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
+    const [token, setToken] = useState(localStorage.getItem('token') || null);
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState('');
 
     const registerUser = async () => {
-      console.log('Отправляемые данные:', { username, password });
-      const response = await fetch('http://85.192.25.173:8080/register', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ username, password })
-      });
-  
-      const data = await response.json();
-      console.log('Ответ от сервера:', data); // Логируем ответ
-      alert(data.message);
-  };
+        const response = await fetch('http://85.192.25.173:8080/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+        const data = await response.json();
+        alert(data.message);
+    };
 
     const loginUser = async () => {
         const response = await fetch('http://85.192.25.173:8080/login', {
@@ -33,11 +30,22 @@ function App() {
         const data = await response.json();
         if (response.ok) {
             setToken(data.token);
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('username', username);
             setIsAuthenticated(true);
             alert('Вход выполнен успешно');
         } else {
             alert(data.message);
         }
+    };
+
+    const logoutUser = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
+        setToken(null);
+        setIsAuthenticated(false);
+        setUsername('');
+        setPassword('');
     };
 
     useEffect(() => {
@@ -83,6 +91,7 @@ function App() {
             ) : (
                 <div>
                     <h1>Чат</h1>
+                    <button onClick={logoutUser}>Выйти</button>
                     <div>
                         {messages.map((msg, index) => (
                             <p key={index}><strong>{msg.username}:</strong> {msg.text}</p>
