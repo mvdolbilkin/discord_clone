52f2c7177a9c0aad650e89f59f857f5b783a6f62
mvdolbilkin <wasdny0@gmail.com>
2025-02-18 21:34:53 +0300

    sad

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { Op } = require('sequelize'); // ✅ Добавляем импорт Op
 const { sequelize, User, Message, Dialog } = require('./database');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
+const jwtDecode = require('jwt-decode');
 const app = express();
 const PORT = 8080;
 const SECRET_KEY = "c366fd93a111ccb4fe1c8cb002c7742f6740a0a09aa7b54e215fcea05ed961b381e6b2d3082eb7879429a616a46df67d6ce76d5d647c29c6b989bbb4c04b8d64"; // ❗ Заменить на переменную окружения в продакшене!
